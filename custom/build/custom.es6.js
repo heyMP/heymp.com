f77a075dd89b2d7455c7e796bbbbbab9b41cf7c8
heyMP <mgp140@gmail.com>
2019-11-02 10:49:14 -0400

    added killswitch-theme and layouts

--- custom/build/custom.es6.js
+++ custom/build/custom.es6.js
@@ -1,4 +1,20 @@
-import{LitElement as e,html as t}from"../../build/es6/node_modules/lit-element/lit-element.js";import"../../build/es6/node_modules/@lrnwebcomponents/haxcms-elements/lib/ui-components/site/site-title.js";import"../../build/es6/node_modules/@lrnwebcomponents/haxcms-elements/lib/ui-components/site/site-breadcrumb.js";customElements.define("killswitch-theme",class extends e{render(){return t`
-      <site-title></site-title>
-      <site-breadcrumb></site-breadcrumb>
-    `}});
+import{PolymerElement as e,html as t}from"../../build/es6/node_modules/@polymer/polymer/polymer-element.js";import{HAXCMSTheme as s}from"../../build/es6/node_modules/@lrnwebcomponents/haxcms-elements/lib/core/HAXCMSThemeWiring.js";import{autorun as o,toJS as i}from"../../build/es6/node_modules/mobx/lib/mobx.module.js";import{store as n}from"../../build/es6/node_modules/@lrnwebcomponents/haxcms-elements/lib/core/haxcms-site-store.js";import{stylesFromTemplate as a}from"../../build/es6/node_modules/@polymer/polymer/lib/utils/style-gather.js";class l extends(s(e)){static get properties(){return{layout:{type:String,value:"default",observer:"__layoutChanged"}}}constructor(){super(),import("../../build/es6/node_modules/@lrnwebcomponents/haxcms-elements/lib/ui-components/active-item/site-active-title.js"),this.__disposer=[],this.__disposer.push(o(()=>{this.__getLayout(i(n.activeItem))})),this.__counter=0,this.getCSS("default"),this.getHTML("default"),window.addEventListener("haxcms-custom-theme-template-ready",this.templateReady.bind(this))}connectedCallback(){super.connectedCallback(),this.contentContainer=this.shadowRoot.querySelector("#contentcontainer")}disconnectedCallback(){this.__disposer.forEach(e=>e()),window.removeEventListener("haxcms-custom-theme-template-ready",this.templateReady.bind(this)),super.disconnectedCallback()}async getCSS(e){return await fetch(`theme/${e}/theme.css`).then(e=>e.text()).then(e=>{const t=new CustomEvent("haxcms-custom-theme-template-ready",{bubbles:!0,composed:!0,cancelable:!1,detail:{css:e}});return this.dispatchEvent(t),e})}async getHTML(e){return await fetch(`theme/${e}/theme.html`).then(e=>e.text()).then(e=>{const t=new CustomEvent("haxcms-custom-theme-template-ready",{bubbles:!0,composed:!0,cancelable:!1,detail:{html:e}});return this.dispatchEvent(t),e})}__layoutChanged(e,t){""!==e&&(this.getCSS(e),this.getHTML(e))}__getLayout(e){e&&void 0!==e.metadata&&void 0!==e.metadata.layout&&e.metadata.layout&&""!==e.metadata.layout&&(this.layout=e.metadata.layout)}templateReady(e){if(this.__counter++,e.detail.css&&(this._css=e.detail.css),e.detail.html&&(this._html=e.detail.html),2===this.__counter){let e=document.createElement("template");e.innerHTML=`\n      <style>\n        /**\n         * Hide the slotted content during edit mode. This must be here to work.\n         */\n        :host([edit-mode]) #slot {\n          display: none;\n        }\n        ${this._css?this._css:""}\n      </style>\n      ${this._html?this._html:""}`;const s=a(e);for(;this.shadowRoot.firstChild;)this.shadowRoot.removeChild(this.shadowRoot.firstChild);for(var t in s)e.innerHTML=s[t].outerHTML+e.innerHTML;this.__instance=this._stampTemplate(e),this.shadowRoot.appendChild(this.__instance)}}static get template(){return t`
+      <style>
+        :host {
+          display: block;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+
+        :host([edit-mode]) #slot {
+          display: none;
+        }
+      </style>
+      <div id="contentcontainer">
+        <div id="slot">
+          <slot></slot>
+        </div>
+      </div>
+    `}}window.customElements.define("killswitch-theme",l);
--- custom/src/killswitch-theme.js
+++ custom/src/killswitch-theme.js
@@ -1,13 +1,176 @@
-import { LitElement, html, css } from 'lit-element/lit-element.js';
-import "@lrnwebcomponents/haxcms-elements/lib/ui-components/site/site-title.js";
-import "@lrnwebcomponents/haxcms-elements/lib/ui-components/site/site-breadcrumb.js";
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXCMSTheme } from "@lrnwebcomponents/haxcms-elements/lib/core/HAXCMSThemeWiring.js";
+import { autorun, toJS } from "mobx/lib/mobx.module.js";
+import { store } from "@lrnwebcomponents/haxcms-elements/lib/core/haxcms-site-store.js";
+import { stylesFromTemplate } from "@polymer/polymer/lib/utils/style-gather.js";
 
-class KillswitchTheme extends LitElement {
-  render() {
+class KillswitchTheme extends HAXCMSTheme(PolymerElement) {
+  static get properties() {
+    return {
+      layout: { type: String, value: "default", observer: "__layoutChanged"}
+    }
+  }
+
+  constructor() {
+    super();
+    import("@lrnwebcomponents/haxcms-elements/lib/ui-components/active-item/site-active-title.js");
+    this.__disposer = []
+    this.__disposer.push(autorun(() => {
+      this.__getLayout(toJS(store.activeItem))
+    }))
+    this.__counter = 0;
+    this.getCSS("default");
+    this.getHTML("default");
+    window.addEventListener(
+      "haxcms-custom-theme-template-ready",
+      this.templateReady.bind(this)
+    );
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.contentContainer = this.shadowRoot.querySelector("#contentcontainer");
+  }
+
+  /**
+   * Detached life cycle
+   */
+  disconnectedCallback() {
+    // this disposes of each item in the disposer
+    this.__disposer.forEach(i => i())
+    window.removeEventListener(
+      "haxcms-custom-theme-template-ready",
+      this.templateReady.bind(this)
+    );
+    super.disconnectedCallback();
+  }
+
+  /**
+   * Get css
+   */
+  async getCSS(layout) {
+    /**
+     * @todo this definitely needs sanitized
+     */
+    return await fetch(`theme/${layout}/theme.css`)
+      .then(response => response.text())
+      .then(response => {
+        const evt = new CustomEvent("haxcms-custom-theme-template-ready", {
+          bubbles: true,
+          composed: true,
+          cancelable: false,
+          detail: {
+            css: response
+          }
+        });
+        this.dispatchEvent(evt);
+
+        return response;
+      });
+  }
+
+  /**
+   * Get css
+   */
+  async getHTML(layout) {
+    /**
+     * @todo this definitely needs sanitized
+     */
+    return await fetch(`theme/${layout}/theme.html`)
+      .then(response => response.text())
+      .then(response => {
+        const evt = new CustomEvent("haxcms-custom-theme-template-ready", {
+          bubbles: true,
+          composed: true,
+          cancelable: false,
+          detail: {
+            html: response
+          }
+        });
+        this.dispatchEvent(evt);
+        return response;
+      });
+  }
+
+  // when the layout changes then we need to rebuild the light dom
+  __layoutChanged(value, oldValue) {
+    if (value === "") return
+    this.getCSS(value)
+    this.getHTML(value)
+  }
+
+  __getLayout(activeItem) {
+    /**
+     * @todo replace with core function that checks for deeply nested objects
+     */
+    if (activeItem) {
+      if (typeof activeItem.metadata !== 'undefined') {
+        if (typeof activeItem.metadata.layout !== 'undefined') {
+          // make sure there is something in there
+          if (activeItem.metadata.layout && activeItem.metadata.layout !== '') {
+            this.layout = activeItem.metadata.layout
+          }
+        }
+      }
+    }
+  }
+
+  templateReady(e) {
+    this.__counter++;
+    if (e.detail.css) {
+      this._css = e.detail.css;
+    }
+    if (e.detail.html) {
+      this._html = e.detail.html;
+    }
+    if (this.__counter === 2) {
+      let t = document.createElement("template");
+      t.innerHTML = `
+      <style>
+        /**
+         * Hide the slotted content during edit mode. This must be here to work.
+         */
+        :host([edit-mode]) #slot {
+          display: none;
+        }
+        ${this._css ? this._css : ""}
+      </style>
+      ${this._html ? this._html : ""}`;
+      const styles = stylesFromTemplate(t);
+      while (this.shadowRoot.firstChild) {
+        this.shadowRoot.removeChild(this.shadowRoot.firstChild);
+      }
+      // add in all styles found
+      for (var i in styles) {
+        t.innerHTML = styles[i].outerHTML + t.innerHTML;
+      }
+      this.__instance = this._stampTemplate(t);
+      // now the template
+      this.shadowRoot.appendChild(this.__instance);
+    }
+  }
+
+  static get template() {
     return html`
-      <site-title></site-title>
-      <site-breadcrumb></site-breadcrumb>
-    `
+      <style>
+        :host {
+          display: block;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+
+        :host([edit-mode]) #slot {
+          display: none;
+        }
+      </style>
+      <div id="contentcontainer">
+        <div id="slot">
+          <slot></slot>
+        </div>
+      </div>
+    `;
   }
 }
-customElements.define('killswitch-theme', KillswitchTheme);
+window.customElements.define("killswitch-theme", KillswitchTheme);
